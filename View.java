1476b1daa70d3a596cf2fd5bb8e5b7c55dbcb6ad
myungah <kna4898@gmail.com>
2015-11-01 14:50:47 +0900

    resolve comfilct of rebase

--- View.java
+++ View.java
@@ -3,6 +3,7 @@ import java.sql.*;
 public class View {
 	int id;
 	String name, department, phone;
+	
 	public View(int id){
 		getStudentInfo(id);
 		display();
@@ -13,28 +14,48 @@ public class View {
 		Statement stmt = null;
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			//Log.i("view", "driver lodding success");
+			//System.out.println("driver lodding success");
 			
-			String url = "jdbc:mysql://localhost";
-			String root = "root";
-			String passwd = "1234";
+			String url = "jdbc:mysql://localhost/StudentInfoTable";
+			String user = "kna4898@gmail.com";
+			String passwd = "Oh!941129or";
 			
-			con = DriverManager.getConnection(url, root, passwd);
-			//Log.i("view", "connection success");
+			con = DriverManager.getConnection(url, user, passwd);
+			//System.out.println("connection success");
 			
-			String sql = "SELECT * FROM table WHERE id = "+ id + ";";
+			String sqlCreate = "CREATE TABLE IF NOT EXISTS StudentInfoTable ("
+					+"id INTEGER NOT NULL,"
+					+"name TEXT(20), "
+					+"phone TEXT(22), "
+					+"PRIMARY KEY(id)"
+					+");";
+			stmt.executeQuery(sqlCreate);
+			String sqlView = "SELECT * FROM table WHERE id = "+ id + ";";
 			stmt = con.createStatement();
 			
-			ResultSet result = stmt.executeQuery(sql);
-			while(result.next()){
+			ResultSet result = stmt.executeQuery(sqlView);
+			boolean isThereAnyData = result.next();
+			if(isThereAnyData==false){
+				this.id = id;
+				name = "no student";
+				department = "no department";
+				phone = "no phone number";
+				
+				//System.out.println("no data");
+			}
+			while(isThereAnyData){
+				this.id = result.getInt("id");
 				name = result.getString("NAME");
 				department = result.getString("DEPARTMENT");
 				phone = result.getString("PHONE");
+				//System.out.println("yes data");
+				
+				isThereAnyData = result.next();
 			}
 			
 			con.close();
 		}catch(Exception e){
-			
+			System.out.println(e);
 		}
 	}
 	
